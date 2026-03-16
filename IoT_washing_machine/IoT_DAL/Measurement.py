cb810ea428ee9f3fae53d6d0436303eb683111e1
MaEmKjJe <10632128@ucn.dk>
2026-03-16 10:00:58 +0100

    Measurement added
    Measurement class added

---
+++ IoT_washing_machine/IoT_DAL/Measurement.py
@@ -0,0 +1,5 @@
+class Measurement:
+    def __init__(self, measurement_id: int, value: float, time):
+        self.measurement_id = measurement_id
+        self.value = value
+        self.time = time
