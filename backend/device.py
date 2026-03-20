1fabcd1f6bcb975b1ad4c364fd7d437f9cf03c8e
MaEmKjJe <10632128@ucn.dk>
2026-03-20 14:28:31 +0100

    Data hentning + API
    Vi kan hente data ned (hvis man har en MQTT broker kørende) og der sendes data til en URL hver gang data bliver hentet ned
    
    Der mangler comments og mere beskrivende try's

--- backend/device.py
+++ backend/device.py
@@ -1,5 +1,5 @@
 class Device: 
-    def init(self, id: int, name: str, type: str):
+    def __init__(self, id: int, name: str, type: str):
         self.id = id
         self.name = name
         self.type = type
